c77ca94031f1b2ae7d042854e74607ffb85c8661
ytsrextest <68838379+ytsrextest@users.noreply.github.com>
2026-09-14 15:06:53 +0530

    Add appmanifest.json, sw.js, loading-logo.png and icon variants for Math Duck

---
+++ html/math_duck/sw.js
@@ -0,0 +1,10 @@
+'use strict';
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
